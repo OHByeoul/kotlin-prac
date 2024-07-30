38645f35c0b890b3e0d0042a9ed789a7412e0188
오한별 <suriessay@naver.com>
2024-07-30 23:58:16 +0900

    test

---
+++ src/test/kotlin/com/group/libraryapp/calculator/CalculatorTest.kt
@@ -0,0 +1,16 @@
+package com.group.libraryapp.calculator
+
+fun main() {
+    val calculatorTest = CalculatorTest()
+    calculatorTest.addTest()
+}
+class CalculatorTest {
+    fun addTest() {
+        val calculator = Calculator(5)
+        calculator.add(3)
+        
+        if (calculator.number != 8) {
+            throw IllegalStateException()
+        }
+    }
+}
