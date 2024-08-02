165fd934a22a86440593bbabfbe40b8c1972f496
오한별 <suriessay@naver.com>
2024-08-02 23:35:50 +0900

    junit 5 코틀린 테스트

--- src/test/kotlin/com/group/libraryapp/calculator1/JunitCalculatorTest.kt
+++ src/test/kotlin/com/group/libraryapp/calculator1/JunitCalculatorTest.kt
@@ -2,6 +2,7 @@ package com.group.libraryapp.calculator1
 
 import org.assertj.core.api.AssertionsForClassTypes.assertThat
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 
 class JunitCalculatorTest {
     @Test
@@ -13,5 +14,37 @@ class JunitCalculatorTest {
         calculator.add(3)
         //then
         assertThat(calculator.number).isEqualTo(8)
+    }
+
+    @Test
+    fun minusTest() {
+        //given
+        val calculator = Calculator(5)
+
+        //when
+        calculator.minus(3)
+        //then
+        assertThat(calculator.number).isEqualTo(2)
+    }
+
+    @Test
+    fun divideExceptionTest(){
+        val calculator = Calculator(5)
+
+        val message = assertThrows<IllegalArgumentException> {
+            calculator.divide(0)
+        }.message
+        assertThat(message).isEqualTo("0으로 나눌 수 없습니다")
+    }
+
+    @Test
+    fun divideExceptionTest2() {
+        val calculator = Calculator(5)
+
+        assertThrows<IllegalArgumentException> {
+            calculator.divide(0)
+        }.apply {
+            assertThat(message).isEqualTo("0으로 나눌 수 없습니다")
+        }
     }
 }
