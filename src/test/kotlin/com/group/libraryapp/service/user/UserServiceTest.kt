6da6d9f758ef63683ba4969c1408dba4a7463d8c
오한별 <suriessay@naver.com>
2024-08-04 11:13:40 +0900

    유저 서비스 테스트 추가 작성

--- src/test/kotlin/com/group/libraryapp/service/user/UserServiceTest.kt
+++ src/test/kotlin/com/group/libraryapp/service/user/UserServiceTest.kt
@@ -3,8 +3,10 @@ package com.group.libraryapp.service.user
 import com.group.libraryapp.domain.user.User
 import com.group.libraryapp.domain.user.UserRepository
 import com.group.libraryapp.dto.user.request.UserCreateRequest
+import com.group.libraryapp.dto.user.request.UserUpdateRequest
 import org.assertj.core.api.AssertionsForInterfaceTypes.assertThat
 import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
@@ -19,6 +21,7 @@ class UserServiceTest @Autowired constructor (
         userRepository.deleteAll()
     }
     @Test
+    @DisplayName("유저저장 정상동작")
     fun saveUserTest() {
         //given
         val request = UserCreateRequest("이름", null)
@@ -47,5 +50,33 @@ class UserServiceTest @Autowired constructor (
         assertThat(results).hasSize(2)
         assertThat(results).extracting("name").containsExactlyInAnyOrder("A","B")
         assertThat(results).extracting("age").containsExactlyInAnyOrder(20,null)
+    }
+
+    @Test
+    @DisplayName("유저 업데이트 정상동작")
+    fun updateUserNameTest() {
+        //given
+        val savedUser = userRepository.save(User("A", null))
+        val request = UserUpdateRequest(savedUser.id, "B")
+
+        //when
+        userService.updateUserName(request)
+
+        //then
+        val result = userRepository.findAll()[0]
+        assertThat(result.name).isEqualTo("B")
+    }
+
+    @Test
+    @DisplayName("유저 삭제 정상동작")
+    fun deleteUserTest() {
+        //given
+        userRepository.save(User("A", null))
+
+        //when
+        userService.deleteUser("A")
+
+        //then
+        assertThat(userRepository.findAll()).isEmpty()
     }
 }
