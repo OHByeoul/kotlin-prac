06623ff50c4dbcaf9993d1c390883ed7d21f6491
오한별 <suriessay@naver.com>
2024-08-20 23:33:02 +0900

    feat : jpql => querydsl로 변경

--- src/main/kotlin/com/group/libraryapp/domain/book/BookRepository.kt
+++ src/main/kotlin/com/group/libraryapp/domain/book/BookRepository.kt
@@ -1,11 +1,12 @@
 package com.group.libraryapp.domain.book
 
+import com.group.libraryapp.domain.user.UserRepositoryCustom
 import com.group.libraryapp.dto.book.response.BookStatResponse
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Query
 import java.util.*
 
-interface BookRepository : JpaRepository<Book, Long> {
+interface BookRepository : JpaRepository<Book, Long>, UserRepositoryCustom {
     fun findByName(bookName: String): Book?
 
     @Query("SELECT new  com.group.libraryapp.dto.book.response.BookStatResponse(b.type, COUNT(b.id)) FROM Book b GROUP By b.type")
